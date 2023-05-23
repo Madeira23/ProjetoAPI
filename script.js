c9f5cbe749e9c96b6bfa0137a746238332599dbb
Madeira23 <diogomgmadeira@gmail.com>
2023-05-23 10:59:39 +0100

    Novas moedas e novo design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Csolana%2Ctron%2Ctether%2Cethereum%2Clitecoin&vs_currencies=eur&include_24hr_change=true";
+const apiKey = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Csolana%2Ctron%2Ctether%2CCardano%2Cethereum%2CDogeCoin%2CUniswap%2Clitecoin&vs_currencies=eur&include_24hr_change=true";
 
 let xhr = new XMLHttpRequest();
 xhr.open("GET", apiKey );
@@ -8,52 +8,62 @@ xhr.onload = function() {
   console.log(result);
   for(const item in result){
     const value = result[`${item}`];
-    const p = document.createElement('p');
+    const divMain = document.createElement('div');
 
+    const logoDiv = document.createElement('div');
+    logoDiv.className = 'coin-logo';
+    const logoImg = document.createElement('img');
+    logoImg.src = `images/${item}.png`;
+    logoImg.style.width = '30px';
+    logoImg.style.height = '30px';
+    logoDiv.appendChild(logoImg);
+    divMain.appendChild(logoDiv);
+    
     // Criar elementos separados para cada parte do texto
     const currencyName = document.createElement('span');
-    currencyName.textContent = item + " ➜    ";
-    currencyName.className = 'currency-name';
+    currencyName.textContent = item;
+    currencyName.className = 'currency-name currency-coin';
 
+    const separator = document.createElement('p');
+    separator.textContent = "➜";
+    separator.className = 'currency-name separator';
+
+    const divValues = document.createElement('div');
+    divValues.classList.add('div-values');
+    
     const eurValue = document.createElement('span');
     eurValue.textContent = value.eur;
     eurValue.className = 'eur-value';
-
+    
     const eurChange = document.createElement('span');
     eurChange.textContent = value.eur_24h_change;
     eurChange.className = 'eur-change';
+    
+    divValues.appendChild(eurValue);
+    divValues.appendChild(eurChange);
 
     // Adicionar os elementos ao parágrafo
-    p.appendChild(currencyName);
-    p.appendChild(eurValue);
-    p.appendChild(eurChange);
+    divMain.appendChild(currencyName);
+    divMain.appendChild(separator);
+    divValues.appendChild(eurValue);
+    divValues.appendChild(eurChange);
 
     // Adicionar o parágrafo ao elemento cnt
-    cnt.appendChild(p);
+    divMain.appendChild(divValues);
+    cnt.appendChild(divMain);
 
     // Adicionar as classes aos elementos conforme necessário
-    p.classList.add('currency');
+    divMain.classList.add('currency');
 
     //let spanMoeda = document.createElement('span');
     //spanMoeda.setAttribute('class', 'moeda');
     //spanMoeda.textContent = item; // nome da moeda
     //p.appendChild(spanMoeda);
 
-    const logoDiv = document.createElement('div');
-    logoDiv.className = 'coin-logo';
-    const logoImg = document.createElement('img');
-    logoImg.src = `images/${item}.png`;
-    logoImg.style.width = '25px';
-    logoImg.style.height = '25px';
-    logoDiv.appendChild(logoImg);
-    p.appendChild(logoDiv);
-
-    
-
     if (value.eur_24h_change < 0) {
-      p.classList.add("negative-change");
+      divMain.classList.add("negative-change");
     } else if (value.eur_24h_change > 0) {
-      p.classList.add("positive-change");
+      divMain.classList.add("positive-change");
     }
   }
 };
